4e4f582683d93cb9c10c374d39ded01d0b7dee4d
AnneLau <1160190085@qq.com>
2020-10-10 13:23:15 +0800

    Add Upload files

---
+++ src/components/Upload/uploadList.tsx
@@ -0,0 +1,46 @@
+import React, { FC } from 'react'
+import { UploadFile } from './upload'
+import Icon from '../Icon/icon'
+import Progress from '../Progress/progress'
+interface UploadListProps {
+  fileList: UploadFile[];
+  onRemove: (_file: UploadFile) => void;
+}
+
+export const UploadList: FC<UploadListProps> = (props) => {
+  const {
+    fileList,
+    onRemove,
+  } = props
+
+  return (
+    <ul className="viking-upload-list">
+      {fileList.map(item => {
+        return (
+          <li className="viking-upload-list-item" key={item.uid}>
+            <span className={`file-name file-name-${item.status}`}>
+              <Icon icon="file-alt" theme="secondary" />
+              {item.name}
+            </span>
+            <span className="file-status">
+              {(item.status === 'uploading' || item.status === 'ready') && <Icon icon="spinner" spin theme="primary" />}
+              {item.status === 'success' && <Icon icon="check-circle" theme="success" />}
+              {item.status === 'error' && <Icon icon="times-circle" theme="danger" />}
+            </span>
+            <span className="file-actions">
+              <Icon icon="times" onClick={() => { onRemove(item)}}/>
+            </span>
+            {item.status === 'uploading' && 
+              <Progress 
+                percent={item.percent || 0}
+              />
+            }
+          </li>
+        )
+      })}
+    </ul>
+  )
+
+}
+
+export default UploadList;
